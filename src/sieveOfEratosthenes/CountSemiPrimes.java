ad7dce17a692ec9f0d6f71afc68456e8df091797
larryjung <corbswjd@gmail.com>
2018-05-03 17:37:04 +0900

    100 solution

--- src/sieveOfEratosthenes/CountSemiPrimes.java
+++ src/sieveOfEratosthenes/CountSemiPrimes.java
@@ -2,17 +2,14 @@ package sieveOfEratosthenes;
 
 public class CountSemiPrimes {
 
-
-    // O(N * log(log(N)) + M * N) or O(M * N**3) or O(M * N ** (3/2))
-    // 55/100/20
     public int[] getSeive(int N) {
-        int[] seive = new int[N+1];
+        int[] seive = new int[N + 1];
         int i = 2;
 
-        while(i * i <= N) {
+        while (i * i <= N) {
             if (seive[i] == 0) {
-                int k = i*i;
-                while(k <= N) {
+                int k = i * i;
+                while (k <= N) {
                     if (seive[k] == 0) {
                         seive[k] = i;
                     } else {
@@ -35,30 +32,14 @@ public class CountSemiPrimes {
         return indexes;
     }
 
-    public boolean isFactorTwo(int n) {
-
-        int[] seive = getSeive(n);
-        //System.out.println(Arrays.toString(seive));
-        int cnt = 0;
-        int i = n;
-
-        while(cnt != 2) {
-            i = i/seive[i];
-            cnt++;
-            if (i == 1) {
-                break;
-            }
-        }
-
-        return i == 1 && cnt == 2 ? true : false;
-    }
-
     public int[] solution(int N, int[] P, int[] Q) {
 
-        int[] numbers = new int[N+1];
+        int[] numbers = new int[N + 1];
+
+        int[] seive = getSeive(N);
 
         for (int i = 4; i < numbers.length; i++) {
-            if (isFactorTwo(i)) {
+            if (seive[i] * seive[i / seive[i]] == i) {
                 numbers[i] = 1;
             }
         }
@@ -66,7 +47,7 @@ public class CountSemiPrimes {
         int[] preSum = new int[numbers.length];
 
         for (int i = 1; i < numbers.length; i++) {
-            preSum[i] = preSum[i-1] + numbers[i];
+            preSum[i] = preSum[i - 1] + numbers[i];
         }
 
         //System.out.println(Arrays.toString(numbers));
@@ -74,12 +55,10 @@ public class CountSemiPrimes {
 
         int[] result = new int[P.length];
 
-        for (int i = 0; i < P.length; i++){
-            result[i] = preSum[Q[i]] - preSum[P[i]-1];
+        for (int i = 0; i < P.length; i++) {
+            result[i] = preSum[Q[i]] - preSum[P[i] - 1];
         }
 
         return result;
     }
-
-
 }
